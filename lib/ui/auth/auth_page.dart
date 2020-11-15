4d4cbced5db26ce87183658a3d82d400026f8a31
iwmh <hiroshiplusone@gmail.com>
2020-11-15 21:40:59 +0900

    implemented code and token exchange with AppAuth for Flutter

--- lib/ui/auth/auth_page.dart
+++ lib/ui/auth/auth_page.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -28,7 +29,7 @@ class AuthPage extends StatelessWidget {
             padding: EdgeInsets.all(20),
           ),
           onTap: () async {
-            await _launchUrl(context);
+            await _auth(context);
           },
         ),
       ),
@@ -36,56 +37,51 @@ class AuthPage extends StatelessWidget {
   }
 }
 
-_launchUrl(BuildContext context) async {
+_auth(BuildContext context) async {
 
   // get secrets from assets folder
   var secrets = await getSecretsFromAssets();
 
-  // parameters required to to authorization code flow with PKCE
-  var CODE_VERIFIER = '';
-  var CODE_CHALLENGE = '';
-  var STATE = '';
+  /**
+   * code and token exchange with AppAuth
+   */
+  FlutterAppAuth appAuth = FlutterAppAuth();
+  final AuthorizationResponse result = await appAuth.authorize(
+    AuthorizationRequest(
+      secrets.clientId,
+      secrets.redirectUrl,
+      serviceConfiguration: AuthorizationServiceConfiguration(
+        'https://accounts.spotify.com/authorize',
+        'https://accounts.spotify.com/api/token'),
+      scopes: ['user-modify-playback-state', 'user-library-modify', 'playlist-read-private', 'playlist-modify-public', 'playlist-modify-private', 'user-read-playback-state', 'user-read-currently-playing']
+    )
+  );
 
-  // string from which random string is created.
-  const random_string_source = 'abcdefghijklmnopqrstuvwxfzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_.-~';
-  const random_string_source_length = random_string_source.length;
+  final TokenResponse tokenResponse = await appAuth.token(
+    TokenRequest(
+        secrets.clientId,
+        secrets.redirectUrl,
+        serviceConfiguration: AuthorizationServiceConfiguration(
+            'https://accounts.spotify.com/authorize',
+            'https://accounts.spotify.com/api/token'),
+        authorizationCode: result.authorizationCode,
+        codeVerifier: result.codeVerifier
+      ),
+    );
 
-  // generate CODE_VERIFIER
-  final rand = new Random();
-  for(var i = 0; i < 128; i++){
-    final n = rand.nextInt(random_string_source_length);
-    CODE_VERIFIER += random_string_source[n];
-  }
+  final TokenResponse refreshTokenResponse = await appAuth.token(
+    TokenRequest(
+        secrets.clientId,
+        secrets.redirectUrl,
+        serviceConfiguration: AuthorizationServiceConfiguration(
+            'https://accounts.spotify.com/authorize',
+            'https://accounts.spotify.com/api/token'),
+        refreshToken: tokenResponse.refreshToken
+    ),
+  );
 
-  // create CODE_CHALLENGER from CODE_VERIFIER
-  CODE_CHALLENGE = createCodeChallengeFromCodeVerifier(CODE_VERIFIER);
-  
-  // generate STATE, 
-  // and store its value to SharedPreferences to use it for comparison, later.
-  for(var i = 0; i < 128; i++){
-    final n = rand.nextInt(random_string_source_length);
-    STATE += random_string_source[n];
-  }
-  final secureStorage = Provider.of<SecureStorage>(context, listen: false);
-  await secureStorage.storeDateToStorage(Constants.PKCE_STATE, STATE);
 
-  var queryParameters = {
-    'client_id': secrets.clientId,
-    'response_type': 'code',
-    'redirect_uri': secrets.redirectUrl,
-    'code_challenge_method': 'S256',
-    'code_challenge': CODE_CHALLENGE,
-    'state': STATE,
-    'scope': 'user-modify-playback-state user-library-modify playlist-read-private playlist-modify-public playlist-modify-private user-read-playback-state user-read-currently-playing',
-  };
-
-  var uri = Uri.https('accounts.spotify.com', '/authorize', queryParameters).toString();
-
-  if(await canLaunch(uri)){
-    await launch(uri);
-  } else {
-    throw 'Could not launch $uri';
-  }
+  print("");
 
 }
 
