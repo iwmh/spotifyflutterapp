f9f8dac1c600b1b7a9ba6b373536276b24d8bd02
iwmh <hiroshiplusone@gmail.com>
2020-11-28 19:06:42 +0900

    add file_storage_repository for unit testing.

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -2,10 +2,15 @@ class Constants {
   // key for local storage.
   static const key_accessToken = "SSA_accessToken";
   static const key_refreshToken = "SSA_refreshToken";
-  static const key_accessTokenExpirationDateTime =
-      "SSA_accessTokenExpirationDateTime";
+  static const key_accessTokenExpirationDateTime = "SSA_accessTokenExpirationDateTime";
 
   // URLs for API call.
-  static const current_users_playlists =
-      'https://api.spotify.com/v1/me/playlists';
+  static const current_users_playlists = 'https://api.spotify.com/v1/me/playlists';
+
+  // when you run tests from console.
+  static const access_token_file = 'file_storage/key_accessToken.txt';
+
+  static const accessTokenExpirationDateTime_file = 'file_storage/key_accessTokenExpirationDateTime.txt';
+
+  static const refresh_token_file = 'file_storage/key_refreshToken.txt';
 }
---
+++ test/file_storage_repository_test.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:spotifyflutterapp/data/repositories/file_storage_repository.dart';
+import 'package:spotifyflutterapp/util/constants.dart';
+
+void main() {
+  var _storage;
+
+  setUp(() {
+    var option;
+    if (Directory.current.path.endsWith('t')) {
+      option = '../';
+    } else {
+      option = '';
+    }
+    _storage = new FileStorage(option);
+  });
+  test('save/read data to/from key_accessToken file.', () async {
+    var data = 'access token';
+    await _storage.storeDataToStorage(Constants.key_accessToken, data);
+    var value = await _storage.readDataFromStorage(Constants.key_accessToken);
+    expect(value, data);
+  });
+
+  test('save/read data to/from key_accessTokenExpirationDateTime file.', () async {
+    var data = 'access token expiration date time';
+    await _storage.storeDataToStorage(Constants.key_accessTokenExpirationDateTime, data);
+    var value = await _storage.readDataFromStorage(Constants.key_accessTokenExpirationDateTime);
+    expect(value, data);
+  });
+
+  test('save/read data to/from key_refreshToken file.', () async {
+    var data = 'refresh token';
+    await _storage.storeDataToStorage(Constants.key_refreshToken, data);
+    var value = await _storage.readDataFromStorage(Constants.key_refreshToken);
+    expect(value, data);
+  });
+}
