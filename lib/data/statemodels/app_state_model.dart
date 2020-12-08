d8f8a52cc5eef4cc87794b192b3674fe4d78b44b
iwmh <hiroshiplusone@gmail.com>
2020-12-09 00:53:24 +0900

    modified to user Router in Navigation 2.0.

--- lib/data/statemodels/app_state_model.dart
+++ lib/data/statemodels/app_state_model.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:spotifyflutterapp/services/api_service.dart';
+import 'package:spotifyflutterapp/ui/auth/auth_page.dart';
+import 'package:spotifyflutterapp/ui/home/home_page.dart';
+import 'package:spotifyflutterapp/ui/tracks/tracks_page.dart';
 
 class AppStateModel extends ChangeNotifier {
   // in-memory expiration date
@@ -58,4 +63,133 @@ class Destination {
   final String title;
   final IconData icon;
   final MaterialColor color;
+}
+
+class AppRoutePath {
+  final String playlistId;
+
+  AppRoutePath.home() : playlistId = null;
+
+  AppRoutePath.playlist(this.playlistId);
+
+  bool get isHomePage => playlistId == null;
+
+  bool get isPlaylistPage => playlistId != null;
+}
+
+class AppRouterDelegate extends RouterDelegate<AppRoutePath>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin<AppRoutePath> {
+  final GlobalKey<NavigatorState> navigatorKey;
+  final BuildContext _context;
+
+  AppRouterDelegate(this._context) : navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // if not logged in, show AuthPage.
+      home: Scaffold(
+        body: Navigator(
+          pages: [
+            // home page.
+            if (Provider.of<ApiService>(context, listen: false).hasLoggedInBefore())
+              MaterialPage(
+                key: ValueKey('HomePage'),
+                child: HomePage(),
+              )
+            else
+              MaterialPage(
+                key: ValueKey('AuthPage'),
+                child: AuthPage(),
+              ),
+
+            // when a playlist is selected
+            if (Provider.of<AppStateModel>(context, listen: false).selectedPlaylistId != '' &&
+                Provider.of<AppStateModel>(context, listen: false).selectedPlaylistId != null)
+              MaterialPage(
+                key: ValueKey('TracksPage'),
+                child: TracksPage(),
+              )
+          ],
+          onPopPage: (route, result) {
+            if (!route.didPop(result)) {
+              return false;
+            }
+            var state = Provider.of<AppStateModel>(context, listen: false);
+            state.selectedPlaylistId = null;
+            return true;
+          },
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: Provider.of<AppStateModel>(context, listen: false).currentIndex,
+          onTap: (int index) {
+            var appState = Provider.of<AppStateModel>(context, listen: false);
+            appState.currentIndex = index;
+          },
+          items: AppStateModel.allDestinations.map((Destination destination) {
+            return BottomNavigationBarItem(
+              icon: Icon(destination.icon),
+              backgroundColor: destination.color,
+              label: destination.title,
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Future<void> setNewRoutePath(AppRoutePath path) {
+    if (path.isPlaylistPage) {
+      Consumer<AppStateModel>(
+        builder: (_, state, child) {
+          state.selectedPlaylistId = path.playlistId;
+        },
+      );
+    } else {
+      Consumer<AppStateModel>(
+        builder: (_, state, child) {
+          state.selectedPlaylistId = null;
+        },
+      );
+    }
+  }
+
+  AppRoutePath get currentConfiguration {
+    var state = Provider.of<AppStateModel>(_context, listen: false);
+    if (state.selectedPlaylistId != '' && state.selectedPlaylistId != null) {
+      return AppRoutePath.playlist(state.selectedPlaylistId);
+    } else {
+      return AppRoutePath.home();
+    }
+  }
+}
+
+class AppRouteInformationParser extends RouteInformationParser<AppRoutePath> {
+  @override
+  Future<AppRoutePath> parseRouteInformation(RouteInformation routeInformation) async {
+    final uri = Uri.parse(routeInformation.location);
+
+    // handler '/'
+    if (uri.pathSegments.length == 0) {
+      return AppRoutePath.home();
+    }
+
+    // handler '/playlist/:id'
+    if (uri.pathSegments.length == 2) {
+      var remaining = uri.pathSegments[1];
+      return AppRoutePath.playlist(remaining);
+    }
+  }
+
+  @override
+  RouteInformation restoreRouteInformation(AppRoutePath path) {
+    if (path.isHomePage) {
+      return RouteInformation(location: '/');
+    }
+    if (path.isPlaylistPage) {
+      return RouteInformation(location: '/playlist/${path.playlistId}');
+    }
+    return null;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,6 @@ import 'package:spotifyflutterapp/data/repositories/secure_storage_repository.da
 import 'package:spotifyflutterapp/data/statemodels/app_state_model.dart';
 import 'package:spotifyflutterapp/data/statemodels/home_state_model.dart';
 import 'package:spotifyflutterapp/services/api_service.dart';
-import 'package:spotifyflutterapp/ui/auth/auth_page.dart';
-import 'package:spotifyflutterapp/ui/home/home_page.dart';
-import 'package:spotifyflutterapp/ui/tracks/tracks_page.dart';
-import 'package:spotifyflutterapp/ui/settings/settings.dart';
 import 'package:spotifyflutterapp/util/util.dart';
 
 void main() async {
@@ -90,84 +86,9 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // if not logged in, show AuthPage.
-      home: Scaffold(
-        body: Navigator(
-          pages: [
-            // home page.
-            if (Provider.of<ApiService>(context, listen: false).hasLoggedInBefore())
-              MaterialPage(
-                key: ValueKey('HomePage'),
-                child: HomePage(),
-              )
-            else
-              MaterialPage(
-                key: ValueKey('AuthPage'),
-                child: AuthPage(),
-              ),
-
-            // when a playlist is selected
-            if (Provider.of<AppStateModel>(context, listen: false).selectedPlaylistId != '')
-              MaterialPage(
-                key: ValueKey('TracksPage'),
-                child: TracksPage(),
-              )
-          ],
-          onPopPage: (route, result) {
-            if (!route.didPop(result)) {
-              return false;
-            }
-            return true;
-          },
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: Provider.of<AppStateModel>(context, listen: false).currentIndex,
-          onTap: (int index) {
-            var appState = Provider.of<AppStateModel>(context, listen: false);
-            appState.currentIndex = index;
-          },
-          items: AppStateModel.allDestinations.map((Destination destination) {
-            return BottomNavigationBarItem(
-              icon: Icon(destination.icon),
-              backgroundColor: destination.color,
-              label: destination.title,
-            );
-          }).toList(),
-        ),
-      ),
+    return MaterialApp.router(
+      routeInformationParser: AppRouteInformationParser(),
+      routerDelegate: AppRouterDelegate(context),
     );
   }
-}
-
-cupertinoTabPage() {
-  return MaterialPage(
-    child: CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'settings')
-          ],
-        ),
-        tabBuilder: (context, index) {
-          switch (index) {
-            case 0:
-              return CupertinoTabView(builder: (context) {
-                return Consumer<HomeStateModel>(
-                  builder: (context, value, child) {
-                    return CupertinoPageScaffold(child: HomePage());
-                  },
-                );
-              });
-            case 1:
-              return CupertinoTabView(builder: (context) {
-                return CupertinoPageScaffold(child: SettingsPage());
-              });
-            default:
-              return CupertinoTabView(builder: (context) {
-                return CupertinoPageScaffold(child: HomePage());
-              });
-          }
-        }),
-  );
 }
