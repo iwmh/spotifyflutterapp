dff2bfc3dbf7d637f61a0f52dd32adf2412c9f55
iwmh <hiroshiplusone@gmail.com>
2021-01-04 20:45:13 +0900

    removed unused class

--- lib/data/statemodels/home_state_model.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:spotifyflutterapp/data/models/playlist.dart';
-
-class HomeStateModel extends ChangeNotifier {
-  List<Playlist> playlist;
-}
